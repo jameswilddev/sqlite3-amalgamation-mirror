977d99b2a8f0eef1b6493af4443a5589fe2ca584
jameswilddev <jameswilddev@outlook.com>
2020-12-30 16:06:32 +0000

    Auto-generated changes from build tools.

--- sqlite3.h
+++ sqlite3.h
@@ -125,7 +125,7 @@ extern "C" {
 */
 #define SQLITE_VERSION        "3.35.0"
 #define SQLITE_VERSION_NUMBER 3035000
-#define SQLITE_SOURCE_ID      "2020-12-29 16:48:54 5774318e7329ca47ead08d53b13f633d50b02f5dd50fd5e3ba4b9302c8b6bca8"
+#define SQLITE_SOURCE_ID      "2020-12-30 13:20:27 45f46317ab8bd92dcd346bf00ba3a33b0cfd030b790c04e19ef33cff124d8d7f"
 
 /*
 ** CAPI3REF: Run-Time Library Version Numbers
